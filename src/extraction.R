84f0321591de523c986216f3729470777a2cd7e8
MauricioACN <alejandrocanomn@gmail.com>
2021-05-07 22:42:11 -0500

    funcion incial de extraccion de info

---
+++ src/extraction.R
@@ -0,0 +1,30 @@
+library(rtweet)
+library(tidyverse)
+source('credenciales/credenciales.R', encoding = 'UTF-8')
+### Functions
+
+extract_tweets <- function(hastags){
+  
+  token_tw = create_token(app = APP_NAME,
+                          consumer_key = TWITTER_API_KEY,
+                          consumer_secret = TWITTER_API_SECRET_KEY,
+                          access_token = TWITTER_ACCES_TOKEN,
+                          access_secret = TWITTER_ACCES_TOKER_SECRET,
+                          set_renv = F
+  )
+  
+  # Validar el correcto funcionamiento de la georeferenciación y el API de google
+  # ColCoord <- lookup_coords(address = "Colombia",
+  #                           components = "country:Colombia",
+  #                           apikey= Sys.getenv(GOOGLE_API_KEY))
+  
+  df = rtweet::search_tweets2(paste('#',hastags,sep = ""),n = 18000,include_rts = F, 
+                                    token = token_tw,retryonratelimit = T)
+  
+  df = df %>% distinct(status_id,.keep_all = T)
+  
+  ### agregar escritura de la base de datos en S3
+  
+  
+}
+
